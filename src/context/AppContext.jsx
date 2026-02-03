98ba3855c81b126a590c8a6dfd219595237abf07
=zofia dobrowolska <z.dobrowolska.993@studms.ug.edu.pl>
2026-02-03 15:48:51 +0100

    feat: implement global app context and state management

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+import initialProducts from '../data/products.json'; 
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+
+  const [products, setProducts] = useLocalStorage('products', initialProducts);
+  const [cart, setCart] = useLocalStorage('cart', []);
+  const [userRole, setUserRole] = useLocalStorage('userRole', 'client'); 
+
+
+  const contextValue = {
+    products, 
+    setProducts,
+    cart, 
+    setCart,
+    userRole, 
+    setUserRole,
+  };
+
+  return (
+    <AppContext.Provider value={contextValue}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hook to use the context
+export const useAppContext = () => {
+  const context = useContext(AppContext)
+  if (!context) {
+    throw new Error('useAppContext must be used within an AppProvider')
+  }
+  return context
+}
